b684048f2a37ba7cf4c470fbd5921d53b3b08a03
PauloAlves8039 <pj.alves547@gmail.com>
2019-11-20 14:35:33 -0300

    Refatorando os arquivos loader e server

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,6 +1,21 @@
 /*
  * Arquivo: server.js
  * Autor: Paulo Alves
- * Descrição: responsável pelo implementação do servidor com Express
+ * Descrição: responsável pela implementação do servidor com Express
  * Data: 20/11/2019
 */
+
+const port = 3003
+
+const bodyParser = require('body-parser')
+const express = require('express')
+const server = express()
+
+server.use(bodyParser.urlencoded({ extended: true }))
+server.use(bodyParser.json())
+
+server.listen(port, function(){
+    console.log(`BACKEND is running on port ${port}.`)
+})
+
+module.exports = server
