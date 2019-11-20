1bdc13c411d80c559e6f7036f5e0607ce66b6959
PauloAlves8039 <pj.alves547@gmail.com>
2019-11-20 14:24:19 -0300

    Adicionando o arquivo loader

---
+++ backend/loader.js
@@ -0,0 +1,19 @@
+/*
+ * Arquivo: loader.js
+ * Autor: Paulo Alves
+ * Descrição: responsável pelo entry point (ponto de entrada) da aplicação
+ * Data: 20/11/2019
+*/
+
+const port = 3003
+
+const bodyParser = require('body-parser')
+const express = require('express')
+const server = express()
+
+server.use(bodyParser.urlencoded({ extended: true }))
+server.use(bodyParser.json())
+
+server.listen(port, function(){
+    console.log(`BACKEND is running on port ${port}.`)
+})
