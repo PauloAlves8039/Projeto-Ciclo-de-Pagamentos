6b7899b0c65ad66f3f1ecfa7313a1055cc5bc226
PauloAlves8039 <pj.alves547@gmail.com>
2019-11-23 01:55:52 -0300

    Adicionando o AngularJs

---
+++ angular1/app/index.js
@@ -0,0 +1,12 @@
+/*
+ * Arquivo: index.js
+ * Autor: Paulo Alves
+ * Descrição: arquivo responsável por adicionar o AngularJS ao projeto
+ * Data: 23/11/2019
+*/
+
+angular.module('primeiraApp', [
+    'ui.router', 
+    'ngAnimate', 
+    'toastr'
+])
