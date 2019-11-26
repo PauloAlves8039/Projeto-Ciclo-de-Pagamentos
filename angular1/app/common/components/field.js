74041201a0fcea70054eac374c7668d826021ca3
PauloAlves8039 <pj.alves547@gmail.com>
2019-11-26 14:32:39 -0300

    Atribuindo operações de atualização e exclusão

--- angular1/app/common/components/field.js
+++ angular1/app/common/components/field.js
@@ -14,6 +14,7 @@
             placeholder: '@',
             type: '@',
             model: '=',
+            readonly: '<',
         },
         controller:[
             'gridSystem',
@@ -26,7 +27,7 @@
                 <div class="form-group">
                     <label for="{{ $ctrl.id }}">{{ $ctrl.label }}</label>
                     <input id="{{ $ctrl.id }}" class="form-control" placeholder="{{ $ctrl.placeholder }}" 
-                    type="{{ $ctrl.type }}" ng-model="$ctrl.model">
+                    type="{{ $ctrl.type }}" ng-model="$ctrl.model" ng-readonly="$ctrl.readonly">
                 </div>
             </div>
         `
