fd256d84ee2ebd80f99e6050fd38d6a64974510c
PauloAlves8039 <pj.alves547@gmail.com>
2019-11-26 00:17:16 -0300

    Configurando mensagens de exibição

--- backend/config/database.js
+++ backend/config/database.js
@@ -8,7 +8,7 @@
 const mongoose = require('mongoose')
 module.exports = mongoose.connect('mongodb://localhost:27017/db_finance', { useCreateIndex: true, useNewUrlParser:true, useUnifiedTopology: true, useFindAndModify: false})
 
-mongoose.Error.messages.general.require = "O atributo '{PATH}' é obrigatório."
+mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório."
 mongoose.Error.messages.Number.min = "O '{VALUE}' informado é menor que o limite mínimo de '{MIN}'."
 mongoose.Error.messages.Number.max = "O '{VALUE}' informado é maior que o limite máximo de '{MAX}'."
 mongoose.Error.messages.String.enum = "'{VALUE}' não é válido para o atributo '{PATH}'."
